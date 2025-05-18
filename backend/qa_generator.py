fe4c79c45104e1f6191fc9e9bbed6101932e0ef2
AkshatKumar99 <kumar.aks99@gmail.com>
2025-05-18 12:29:56 -0500

    fixed inputs to methods

--- backend/qa_generator.py
+++ backend/qa_generator.py
@@ -1,17 +1,17 @@
 
 def generate_qa_pairs(chunks, debugging=False):
     
-    if not debugging:
-        pass
-    else:
+    if debugging:
         _debug_return()
-    
-def generate_qa_from_llm_only(debugging=False):
-    
-    if not debugging:
-        pass
     else:
+        pass
+    
+def generate_qa_from_llm_only(topic, debugging=False):
+    
+    if debugging:
         _debug_return()
+    else:
+        pass
         
 def _debug_return():
     return [("Question 1", "Answer 1"), ("Question 2", "Answer 2")]
